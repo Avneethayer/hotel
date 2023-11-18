b9f73ed8d268cbd2929ffc31a0fc35b42e9d05c4
Avneet Kaur <avneet.hayer1@gmail.com>
2023-11-17 18:49:54 -0600

    done adding custom classes for bg color and fonts for website

--- hotelpro/src/App.jsx
+++ hotelpro/src/App.jsx
@@ -1,8 +1,21 @@
 //import React from "react";
 import "./App.css";
 
+//arbitary values, defining own styles
+// const bgColor = "bg-green-500";
+const bg_light_beige = "bg-my-light-beige";
+const bg_brown = "bg-my-brown";
+
 const App = () => {
-  return <div className="underline">App</div>;
+  return (
+    <div className={`underline ${bg_brown}`}>
+      App
+      <div className="bg_brown">
+        <p className="font-kalam">This text uses kalam.</p>
+        <p className="font-sans">This text uses the default open sansfont.</p>
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- hotelpro/tailwind.config.js
+++ hotelpro/tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        "my-light-beige": "#FDF8F0",
+        "my-brown": "#99775C",
+      },
+      fontFamily: {
+        kalam: ["Kalam", "cursive"],
+        sans: ["Open Sans", "sans-serif"],
+        raleway: ["Raleway", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
